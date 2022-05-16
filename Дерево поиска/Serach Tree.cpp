19d8c82f4d8600b6c03e7c87db65cd3b5b73d54e
Данил Калеев <KaleevDA@stud.kai.ru>
2022-05-16 22:20:36 +0300

    Готовый вариант без пользовательского интерфейса

--- Дерево поиска/Serach Tree.cpp
+++ Дерево поиска/Serach Tree.cpp
@@ -1,78 +1,177 @@
-//#include "iostream"
-//
-//
-//struct Search_Tree
-//{
-//	int key;
-//	int count;
-//	std::string data;
-//
-//
-//	Search_Tree* left = nullptr;
-//	Search_Tree* right = nullptr;
-//};
-//
-//void Random_string(Search_Tree* pCurrent)
-//{
-//	std::string temp_str = "";
-//
-//	for (size_t i = 0; i < (rand() % 15 + 5); i++)
-//	{
-//		int temp = rand() % 26 + 97;
-//		temp_str += static_cast<char>(temp);
-//	}
-//	pCurrent->data = temp_str;
-//}
-//
-//
-//Search_Tree* Non_Rec_Search(Search_Tree*& previous, int key)
-//{
-//	Search_Tree* Temp = previous;
-//
-//	while (Temp)
-//	{
-//		if (key == Temp->key) { return Temp; }
-//		
-//		previous = Temp;
-//		if (key < Temp->key) {   Temp = Temp->left; }
-//		else if (key > Temp->key) { Temp = Temp->right; }
-//	}
-//	return Temp;
-//}
-//
-//
-//void Add_Node(Search_Tree*& pCurrent, int key)
-//{
-//	if (pCurrent == nullptr)
-//	{
-//		pCurrent = new Search_Tree;
-//		pCurrent->key = key;
-//		pCurrent->count = 1;
-//
-//		pCurrent->left = nullptr;
-//		pCurrent->right = nullptr;
-//	}
-//	else
-//	{
-//		if (key < pCurrent->key) { Add_Node(pCurrent->left, key); }
-//		else if (key > pCurrent->key) { Add_Node(pCurrent->right, key); }
-//		else { pCurrent->count++; }
-//	}
-//}
-//
-//void Add_Node_NoR(Search_Tree*& Root, int key)
-//{
-//
-//	Search_Tree* pCurrent = Root;
-//	pCurrent = Non_Rec_Search(pCurrent, key);
-//
-//	pCurrent = new Search_Tree;
-//	pCurrent->key = key;
-//	pCurrent->count = 1;
-//
-//	pCurrent->left = nullptr;
-//	pCurrent->right = nullptr;
-//
-//	if (!Root) { Root = pCurrent; }
-//
-//}
+#include <iostream>
+
+struct Search_Tree
+{
+	int key;
+	int count;
+	std::string data;
+
+
+	Search_Tree* left = nullptr;
+	Search_Tree* right = nullptr;
+};
+
+std::string Random_string()
+{
+	std::string temp_str = "";
+
+	for (size_t i = 0; i < (rand() % 15 + 5); i++)
+	{
+		int temp = rand() % 26 + 97;
+		temp_str += static_cast<char>(temp);
+	}
+	return temp_str;
+}
+
+
+
+void Add_Node(Search_Tree*& pCurrent, int key)
+{
+	if (pCurrent == nullptr)
+	{
+		pCurrent = new Search_Tree;
+		pCurrent->key = key;
+		pCurrent->count = 1;
+
+		pCurrent->left = nullptr;
+		pCurrent->right = nullptr;
+	}
+	else
+	{
+		if (key < pCurrent->key) { Add_Node(pCurrent->left, key); }
+		else if (key > pCurrent->key) { Add_Node(pCurrent->right, key); }
+		else { pCurrent->count++; }
+	}
+}
+
+void Changer(Search_Tree*& pCurrent, Search_Tree*& pTemp)
+{
+	if (pCurrent->right)
+	{
+		Changer(pCurrent->right, pTemp);
+	}
+	else
+	{
+		pTemp->key = pCurrent->key;
+		pTemp->data = pCurrent->data;
+		pTemp = pCurrent;
+		pCurrent = pCurrent->left;
+	}
+}
+
+void Delete_Node(Search_Tree*& pCurrent, int key)
+{
+	Search_Tree* Temp = nullptr;
+
+	if (pCurrent)
+	{
+		if (key < pCurrent->key) { Delete_Node(pCurrent->left, key); }
+		else if (key > pCurrent->key) { Delete_Node(pCurrent->right, key); }
+		else
+		{
+			Temp = pCurrent;
+			if (Temp->right == nullptr) { pCurrent = Temp->left; }
+			else if (Temp->left == nullptr) { pCurrent = Temp->right; }
+			else { Changer(pCurrent->left, Temp); }
+			delete Temp;
+		}
+	}
+
+}
+
+
+
+Search_Tree** Non_Rec_Search(Search_Tree* head, int key)
+{
+	Search_Tree** Temp = &head;
+
+	while (*Temp)
+	{
+		if (key == (*Temp)->key) { return Temp; }
+
+		if (key < (*Temp)->key) { Temp = &(*Temp)->left; }
+		else if (key > (*Temp)->key) { Temp = &(*Temp)->right; }
+	}
+	return Temp;
+}
+
+void Add_Node_NoR(Search_Tree*& Root, int key)
+{
+	Search_Tree** pCurrent = nullptr;
+
+	if (Root == nullptr) { pCurrent = &Root; }
+	else
+	{
+		pCurrent = Non_Rec_Search(Root, key);
+
+		if (*pCurrent) { (*pCurrent)->count++; return; }
+	}
+
+	(*pCurrent) = new Search_Tree;
+	(*pCurrent)->key = key;
+	(*pCurrent)->count = 1;
+	//(*pCurrent)->data = Random_string();
+
+	(*pCurrent)->left = nullptr;
+	(*pCurrent)->right = nullptr;
+}
+
+void Delete_Node_NoR(Search_Tree*& Root, int key)
+{
+	Search_Tree** pCurrent = Non_Rec_Search(Root, key);
+	if (pCurrent == nullptr) { return; }
+
+	Search_Tree* Temp = *pCurrent;
+	if (Temp->right == nullptr) { *pCurrent = Temp->left; }
+	else if (Temp->left == nullptr) { *pCurrent = Temp->right; }
+	else
+	{
+		pCurrent = &(*pCurrent)->left;
+		while ((*pCurrent)->right) { pCurrent = &(*pCurrent)->right; }
+		Temp->key = (*pCurrent)->key;
+		Temp->data = (*pCurrent)->data;
+		Temp = *pCurrent;
+		*pCurrent = (*pCurrent)->left;
+	}
+	delete Temp;
+}
+
+
+void clean_memory(Search_Tree* current)
+{
+	if (current != nullptr)
+	{
+
+		clean_memory(current->left);
+		clean_memory(current->right);
+		delete current;
+	}
+}
+
+void forward(Search_Tree* current, int count)
+{
+	if (current != nullptr)
+	{
+		for (size_t i = 0; i < count; i++)
+		{
+			std::cout << "\t";
+		}
+		std::cout << current->key << std::endl;
+		forward(current->left, count + 1);
+		forward(current->right, count + 1);
+	}
+}
+
+void symmetric(Search_Tree* current, int count)
+{
+	if (current != nullptr)
+	{
+		symmetric(current->left, count + 1);
+		for (size_t i = 0; i < count; i++)
+		{
+			std::cout << "\t";
+		}
+		std::cout << current->key << std::endl;
+		symmetric(current->right, count + 1);
+	}
+}
